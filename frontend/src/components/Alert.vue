4bebea944258de0c323e28a3180b828564780bf3
Iris Kotsinas <iris.kotsinas@gmail.com>
2021-05-15 16:59:23 +0200

    add styles to alert and modal

--- frontend/src/components/Alert.vue
+++ frontend/src/components/Alert.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="alert">
-    <button 
+    <div 
+      class="cancel"
       @click="$emit('show')"
     >
-      Close Window
-    </button>
-    <button 
+      X
+    </div>
+    Are you sure you want to {{ action }} this session?
+    <button
+      class="delete-btn"
       @click="onAction"
     >
       {{ action.toUpperCase() }}
@@ -36,16 +39,92 @@ export default {
   position: fixed;
   top: 50%;
   left: 50%;
-  width: 60vw;
-  height: 60vh;
+  width: 25em;
+  height: 10em;
   transform: translate(-50%, -50%);
-  background: white;
-  color: black;
+  background-color: $grey-color;
   z-index: 1000;
+  display: flex;
+  flex-direction: column;
 }
 
-.container {
-    color: black;
+button {
+  box-sizing: border-box;
+  appearance: none;
+  background-color: black !important;
+  border: 2px solid #fff;
+  border-radius: 0.6em;
+  color: #fff;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1;
+  padding: 0.5em 0.5em;
+  width: 10em;
+  height: 3em;
+  text-decoration: none;
+  text-align: center;
+  text-transform: uppercase;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  margin: 0 1.5em;
+  
+  &:after {
+    content: '';
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    /* transform: translateX(-50%); */
+    width: 0;
+    height: 100%;
+    background: red;
+    z-index: -1;
+    transition: width 150ms ease-in-out;
+  }
+  
+  &:hover {
+    color: #000;
+    &:after {
+      width: 110%;
+    }
+  }
+}
+
+.cancel {
+  text-align: right;
+  margin: 1em 1em 0 0;
+  color: red;
+  z-index: 1;
+  font-size: 20px;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.delete-btn {
+  border-color: red;
+  margin: 1em auto;
+  border-radius: 0.1;
+  color: white;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  transition: color 150ms ease-in-out;
+  background: red !important;
+
+  &:after {
+    background: black;
+  }
+  
+  &:hover {
+    color: red;
+    &:after {
+      width: 110%;
+    }
+  }
 }
 
 </style>
--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="modal">
-    <button
-      class="close"
+    <div
+      class="cancel"
       @click="$emit('show')"
     >
-      CLOSE HERE
-    </button>
+      X
+    </div>
+    <div class="userHeading">
+      Users signed up for session:
+    </div>
     <div
       v-for="user in users"
       :key="user._id"
       class="container"
     >
-      {{ user.firstname }}
-      {{ user.lastname }}
-      {{ user.email }}
+      <div class="user">
+        {{ user.firstname }} {{ user.lastname }}
+      </div>
+      <div class="user">
+        {{ user.email }}
+      </div>
     </div>
   </div>
 </template>
@@ -36,16 +42,40 @@ export default {
   position: fixed;
   top: 50%;
   left: 50%;
-  width: 60vw;
-  height: 60vh;
+  width: 50vw;
+  height: 50vh;
   transform: translate(-50%, -50%);
+  padding: 2em;
   background: white;
   color: black;
   z-index: 1000;
+  background-color: $grey-color;
 }
 
 .container {
-    color: black;
+  color: white;
+  display: flex;
+  justify-content: space-evenly;
+  padding-bottom: 0.5em;
+  border-bottom: 1px solid white;
+}
+
+.userHeading {
+  font-weight: bold;
+  color: white;
+  margin-bottom: 1em;
+}
+
+.cancel {
+  text-align: right;
+  color: red;
+  z-index: 1;
+  font-size: 30px;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+  }
 }
 
 </style>
